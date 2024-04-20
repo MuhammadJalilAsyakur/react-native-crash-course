04516de4d852d7340fe2c0e16ae75163b89b89bc
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-04-20 22:22:55 +0700

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RooyLayout = () => {
+    return (
+        <View>
+            <Text>RooyLayout</Text>
+        </View>
+    )
+}
+
+export default RooyLayout
+
+const styles = StyleSheet.create({})
