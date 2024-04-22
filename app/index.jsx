03f72419a479ccbfa780c14de57b189369ba8fcd
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-04-22 14:23:24 +0700

    add assets and constants and add useEffect for fonts if doesnt show the screen is gonna be blank

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router';
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text>Hello world</Text>
+            <Text className="font-pbold">Hello world</Text>
             <StatusBar style="auto" />
             <Link href="/profile" className='text-blue-500'>
                 Go to Profile</Link>
