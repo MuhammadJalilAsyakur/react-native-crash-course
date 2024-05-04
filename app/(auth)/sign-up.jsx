5d16ddc2f9c88717e6973b43445cae99e9407330
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2024-05-04 22:41:05 +0700

    add some function but there is still an error

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, Alert } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
@@ -7,8 +7,9 @@ import { images } from '../../constants'
 import FormField from '../../components/FormField'
 import CustomButton from '../../components/CustomButton'
 
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 import { createUser } from '../../lib/appwrite'
+
 const SignUp = () => {
     const [form, setForm] = useState({
         username: '',
@@ -18,7 +19,27 @@ const SignUp = () => {
 
     const [isSubmitting, setIsSubmitting] = useState(false)
 
-    const submit = () => {
+    const submit = async () => {
+        if (!form.username || !form.email || !form.password) {
+            Alert.alert('Error', 'Please fill all fields')
+        }
+
+        setIsSubmitting(true)
+
+        try {
+            const result = await createUser(form.email, form.password,
+                form.username)
+
+
+            router.replace('/home')
+        } catch (error) {
+            Alert.alert('Error', error.message)
+        } finally {
+            setIsSubmitting(false)
+        }
+
+
+
         createUser();
     }
     return (
@@ -64,7 +85,7 @@ const SignUp = () => {
                     />
 
                     <CustomButton
-                        title='sign in'
+                        title='sign up'
                         handlePress={submit}
                         containerStyles='mt-7'
                         isLoading={isSubmitting}
